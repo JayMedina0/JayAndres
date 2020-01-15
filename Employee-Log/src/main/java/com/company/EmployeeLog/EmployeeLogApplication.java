815be491ce5cb37342058f26c2ed155ef90bc7b7
JayMedina <jay.medina813@gmail.com>
2020-01-15 13:51:34 -0500

    initial commit

--- Employee-Log/src/main/java/com/company/EmployeeLog/EmployeeLogApplication.java
+++ Employee-Log/src/main/java/com/company/EmployeeLog/EmployeeLogApplication.java
@@ -2,8 +2,16 @@ package com.company.EmployeeLog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableHystrix
+@EnableWebSecurity
 public class EmployeeLogApplication {
 
 	public static void main(String[] args) {
---
+++ Employee-Log/src/main/java/com/company/EmployeeLog/controller/EmployeeLogController.java
@@ -0,0 +1,4 @@
+package com.company.EmployeeLog.controller;
+
+public class EmployeeLogController {
+}
