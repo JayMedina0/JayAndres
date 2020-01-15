febb7ed59d0a99e852c59dfaa7e4104fd2d4aa12
JayMedina <jay.medina813@gmail.com>
2020-01-15 13:37:17 -0500

    initial commit

---
+++ Employee-Log/src/main/java/com/company/EmployeeLog/EmployeeLogApplication.java
@@ -0,0 +1,13 @@
+package com.company.EmployeeLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeLogApplication.class, args);
+	}
+
+}
